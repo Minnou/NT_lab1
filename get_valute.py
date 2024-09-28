62b241b659f65c991cf05b531b19cb2e999bf231
Minnou <minnouser@gmail.com>
2024-09-28 20:07:51 +0300

    Changed valute. Minor changes.

--- get_valute.py
+++ get_valute.py
@@ -1,34 +1,24 @@
 import requests
 import time
 import re
-from http import HTTPStatus
 
-retry_codes = [
-    HTTPStatus.TOO_MANY_REQUESTS,
-    HTTPStatus.INTERNAL_SERVER_ERROR,
-    HTTPStatus.BAD_GATEWAY,
-    HTTPStatus.SERVICE_UNAVAILABLE,
-    HTTPStatus.GATEWAY_TIMEOUT,
-]
-
-days = 5
+days = 100
 i = 1
 https = "https:"
 currentUrl = "//www.cbr-xml-daily.ru/daily_json.js"
-resultFile = open("./usd.csv", "w+", encoding="utf-8")
+resultFile = open("./dataset.csv", "w+", encoding="utf-8")
 retries = 0
 maxRetries = 3
 timeDelay = 0.7
 
 while (i <= days):
     try:
-        rawData = requests.get(https + currentUrl)
+        rawData = requests.get(https + currentUrl, timeout=30)
         rawData.raise_for_status()
-    except requests.HTTPError as exc:
-        code = exc.response.status_code
-        
-        if code in retry_codes and retries < maxRetries:
-            print("Something went wrong, retrying. retry #" + retries)
+    except:
+        print("Something went wrong. ",end="")
+        if retries < maxRetries:
+            print("Retrying. retry #" + str(retries))
             retries += 1
             time.sleep(5)
             continue
@@ -38,7 +28,7 @@ while (i <= days):
     retries = 0
     resultJSON = rawData.json()
     currentUrl = resultJSON["PreviousURL"]
-    value = resultJSON["Valute"]["USD"]["Value"]
+    value = resultJSON["Valute"]["JPY"]["Value"]
     date = re.match(pattern="(.*)T.*", string=resultJSON["Date"]).group(1)
     resultFile.write(str(date) + ";" + str(value) + "\n")
     print("day #" + str(i) + ". days remaining: " + str(days - i) + ". date: "+ str(date) + ". value: " + str(value) + "\n" )
